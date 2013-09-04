7aa0cce9b664a8a9131bc2efa0cf65fa8305ff6d
NikSan <nikulinaleksandr@gmail.com>
2013-09-04 18:30:16 +0400

    verstka in progress

--- db/schema.rb
+++ db/schema.rb
@@ -9,84 +9,84 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130722183027) do
+ActiveRecord::Schema.define(version: 20130722183027) do
 
-  create_table "articles", :force => true do |t|
+  create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string   "ancestry"
     t.string   "slug"
     t.integer  "position"
   end
 
-  add_index "articles", ["ancestry"], :name => "index_articles_on_ancestry"
-  add_index "articles", ["slug"], :name => "index_articles_on_slug"
+  add_index "articles", ["ancestry"], name: "index_articles_on_ancestry", using: :btree
+  add_index "articles", ["slug"], name: "index_articles_on_slug", using: :btree
 
-  create_table "categories", :force => true do |t|
+  create_table "categories", force: true do |t|
     t.string   "name"
     t.string   "slug"
     t.string   "ancestry"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer  "position"
   end
 
-  add_index "categories", ["ancestry"], :name => "index_categories_on_ancestry"
-  add_index "categories", ["slug"], :name => "index_categories_on_slug"
+  add_index "categories", ["ancestry"], name: "index_categories_on_ancestry", using: :btree
+  add_index "categories", ["slug"], name: "index_categories_on_slug", using: :btree
 
-  create_table "ckeditor_assets", :force => true do |t|
-    t.string   "data_file_name",                  :null => false
+  create_table "ckeditor_assets", force: true do |t|
+    t.string   "data_file_name",               null: false
     t.string   "data_content_type"
     t.integer  "data_file_size"
     t.integer  "assetable_id"
-    t.string   "assetable_type",    :limit => 30
-    t.string   "type",              :limit => 30
+    t.string   "assetable_type",    limit: 30
+    t.string   "type",              limit: 30
     t.integer  "width"
     t.integer  "height"
-    t.datetime "created_at",                      :null => false
-    t.datetime "updated_at",                      :null => false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
   end
 
-  add_index "ckeditor_assets", ["assetable_type", "assetable_id"], :name => "idx_ckeditor_assetable"
-  add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], :name => "idx_ckeditor_assetable_type"
+  add_index "ckeditor_assets", ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable", using: :btree
+  add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type", using: :btree
 
-  create_table "friendly_id_slugs", :force => true do |t|
-    t.string   "slug",                         :null => false
-    t.integer  "sluggable_id",                 :null => false
-    t.string   "sluggable_type", :limit => 40
+  create_table "friendly_id_slugs", force: true do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 40
     t.datetime "created_at"
   end
 
-  add_index "friendly_id_slugs", ["slug", "sluggable_type"], :name => "index_friendly_id_slugs_on_slug_and_sluggable_type", :unique => true
-  add_index "friendly_id_slugs", ["sluggable_id"], :name => "index_friendly_id_slugs_on_sluggable_id"
-  add_index "friendly_id_slugs", ["sluggable_type"], :name => "index_friendly_id_slugs_on_sluggable_type"
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", unique: true, using: :btree
+  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
 
-  create_table "gritter_notices", :force => true do |t|
-    t.integer  "owner_id",     :null => false
-    t.string   "owner_type",   :null => false
-    t.text     "text",         :null => false
-    t.text     "options",      :null => false
+  create_table "gritter_notices", force: true do |t|
+    t.integer  "owner_id",     null: false
+    t.string   "owner_type",   null: false
+    t.text     "text",         null: false
+    t.text     "options",      null: false
     t.datetime "delivered_at"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "gritter_notices", ["owner_id", "delivered_at"], :name => "index_gritter_notices_on_owner_id_and_delivered_at"
+  add_index "gritter_notices", ["owner_id", "delivered_at"], name: "index_gritter_notices_on_owner_id_and_delivered_at", using: :btree
 
-  create_table "order_items", :force => true do |t|
+  create_table "order_items", force: true do |t|
     t.integer  "order_id"
     t.integer  "product_id"
     t.integer  "quantity"
     t.decimal  "price"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "orders", :force => true do |t|
+  create_table "orders", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.string   "phone"
@@ -94,79 +94,79 @@ ActiveRecord::Schema.define(:version => 20130722183027) do
     t.string   "email"
     t.text     "additional"
     t.string   "state"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "photos", :force => true do |t|
+  create_table "photos", force: true do |t|
     t.integer  "product_id"
     t.string   "image"
     t.string   "ancestry"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer  "position"
   end
 
-  add_index "photos", ["product_id"], :name => "index_photos_on_product_id"
+  add_index "photos", ["product_id"], name: "index_photos_on_product_id", using: :btree
 
-  create_table "products", :force => true do |t|
+  create_table "products", force: true do |t|
     t.integer  "category_id"
     t.string   "lot"
     t.string   "name"
     t.text     "description"
-    t.decimal  "price",            :precision => 7, :scale => 2, :default => 0.0
+    t.decimal  "price",            precision: 7, scale: 2, default: 0.0
     t.string   "ancestry"
     t.string   "slug"
     t.text     "related_products"
-    t.boolean  "disabled",                                       :default => false
-    t.datetime "created_at",                                                        :null => false
-    t.datetime "updated_at",                                                        :null => false
+    t.boolean  "disabled",                                 default: false
+    t.datetime "created_at",                                               null: false
+    t.datetime "updated_at",                                               null: false
     t.integer  "position"
   end
 
-  add_index "products", ["ancestry"], :name => "index_products_on_ancestry"
-  add_index "products", ["slug"], :name => "index_products_on_slug"
+  add_index "products", ["ancestry"], name: "index_products_on_ancestry", using: :btree
+  add_index "products", ["slug"], name: "index_products_on_slug", using: :btree
 
-  create_table "rails_admin_histories", :force => true do |t|
+  create_table "rails_admin_histories", force: true do |t|
     t.text     "message"
     t.string   "username"
     t.integer  "item"
     t.string   "table"
-    t.integer  "month",      :limit => 2
-    t.integer  "year",       :limit => 8
-    t.datetime "created_at",              :null => false
-    t.datetime "updated_at",              :null => false
+    t.integer  "month",      limit: 2
+    t.integer  "year",       limit: 8
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
   end
 
-  add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
+  add_index "rails_admin_histories", ["item", "table", "month", "year"], name: "index_rails_admin_histories", using: :btree
 
-  create_table "roles", :force => true do |t|
+  create_table "roles", force: true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "roles_users", :id => false, :force => true do |t|
+  create_table "roles_users", id: false, force: true do |t|
     t.integer "role_id"
     t.integer "user_id"
   end
 
-  create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
