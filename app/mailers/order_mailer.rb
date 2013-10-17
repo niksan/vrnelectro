3cab3fb9531dc921d243ab5a7ed787159577efae
NikSan <nikulinaleksandr@gmail.com>
2013-10-17 14:02:22 +0400

    change order email

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -4,7 +4,7 @@ class OrderMailer < ActionMailer::Base
   def order(order_datas, cart_datas)
     @order_datas = order_datas
     @cart_datas = cart_datas
-    mail(to: 'internet36@mail.ru', subject: 'Заказ из интернет-магазина.')
+    mail(to: 'vrnelectro@mail.ru', subject: 'Заказ из интернет-магазина.')
   end
 
 end
