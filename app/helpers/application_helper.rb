71775ecd210bf8b2d896fd6a83f30d9401649a35
NikSan <nikulinaleksandr@gmail.com>
2013-11-02 11:39:04 +0400

    add submenu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,14 @@ module ApplicationHelper
     raw(obj.errors.full_messages.map{ |msg| content_tag(:div, content_tag(:button, raw('&times;'), class: 'close', :'data-dismiss' => 'alert') + msg, class: 'alert alert-error') }.join) if obj
   end
 
+  def submunu(parent)
+    if parent.has_children?
+      content_tag :ul do
+        parent.children.map do |children|
+          concat(content_tag :li, link_to(children.name, category_path(children)))
+        end
+      end
+    end
+  end
+
 end
